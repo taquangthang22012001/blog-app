ceba1fa90247dec2fdcd535d0703db79461c8075
thang <thang220101@gmail.com>
2023-12-02 21:15:39 +0700

    [Add] Post service

---
+++ src/main/java/com/vti/blogapp/service/PostService.java
@@ -0,0 +1,9 @@
+package com.vti.blogapp.service;
+
+import com.vti.blogapp.dto.PostDto;
+import com.vti.blogapp.form.PostCreateForm;
+
+
+public interface PostService {
+    PostDto create(PostCreateForm form);
+}
