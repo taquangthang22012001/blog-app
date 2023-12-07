69be93fb0ddff3930450878d80dad4c6a1e2d078
thang <thang220101@gmail.com>
2023-12-07 20:29:25 +0700

    [Add] Comment Dto

---
+++ src/main/java/com/vti/blogapp/dto/CommentDto.java
@@ -0,0 +1,18 @@
+package com.vti.blogapp.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+public class CommentDto {
+
+    private Long id;
+    private String name;
+    private String email;
+    private String body;
+    private LocalDateTime updatedAt;
+    private LocalDateTime createdAt;
+}
