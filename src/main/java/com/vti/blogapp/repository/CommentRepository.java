90e90340296ee8dc3c673f53d9126789fb445781
thang <thang220101@gmail.com>
2023-12-12 18:18:46 +0700

    [Add] Find Comment by post API

--- src/main/java/com/vti/blogapp/repository/CommentRepository.java
+++ src/main/java/com/vti/blogapp/repository/CommentRepository.java
@@ -1,7 +1,10 @@
 package com.vti.blogapp.repository;
 
 import com.vti.blogapp.entity.Comment;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+    Page<Comment> findByPostId(Long postId, Pageable pageable);
 }
