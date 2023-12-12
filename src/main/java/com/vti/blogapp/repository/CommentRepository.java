320d76e30b66a7b6259bc0ba22e2fc43534556ae
thang <thang220101@gmail.com>
2023-12-12 18:47:17 +0700

    [Add] Delete comment by email API

--- src/main/java/com/vti/blogapp/repository/CommentRepository.java
+++ src/main/java/com/vti/blogapp/repository/CommentRepository.java
@@ -4,7 +4,20 @@ import com.vti.blogapp.entity.Comment;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
-    Page<Comment> findByPostId(Long postId, Pageable pageable);
+   // Page<Comment> findByPostId(Long postId, Pageable pageable);
+
+    // @ Query annotation
+    //@Query("FROM Comment WHERE postId = ?1")
+    //@Query("FROM Comment WHERE postId = :postId")
+    @Query(value = "SELECT * FROM comment WHERE post_id = :postId", nativeQuery = true)
+    Page<Comment> findByPostId (@Param("postId") Long postId, Pageable pageable);
+
+    @Query("DELETE Comment WHERE email = ?1")
+    @Modifying
+    void deleteByEmail(String email);
 }
--- src/main/java/com/vti/blogapp/service/CommentService.java
+++ src/main/java/com/vti/blogapp/service/CommentService.java
@@ -15,4 +15,6 @@ public interface CommentService {
     CommentDto update(CommentUpdateForm form, Long id);
 
     void deleteById(Long id);
+
+    void deleteByEmail(String email);
 }
