a49751f8afdc5283acc4791621d348b3002d8a13
thang <thang220101@gmail.com>
2023-12-12 21:50:01 +0700

    [Add] Validate PostUpdateForm

--- src/main/java/com/vti/blogapp/form/PostUpdateForm.java
+++ src/main/java/com/vti/blogapp/form/PostUpdateForm.java
@@ -1,12 +1,25 @@
 package com.vti.blogapp.form;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
+import org.springframework.validation.annotation.Validated;
 
+@Validated
 @Getter
 @Setter
 public class PostUpdateForm {
+
+    @NotBlank
+    @Length(max = 50)
     private String title;
+
+    @NotBlank
+    @Length(max = 100)
     private String description;
+
+    @NotBlank
+    @Length(max = 150)
     private String content;
 }
